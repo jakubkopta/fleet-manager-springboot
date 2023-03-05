b6b883e7963e2662c77c3133f15494e88041fe6f
Jakub Kopta <jakubkopta@gmail.com>
2023-03-05 15:31:08 +0100

    Initial commit

---
+++ src/main/java/com/project/fleetmanagerspringboot/FleetManagerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.project.fleetmanagerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagerSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetManagerSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/fleetmanagerspringboot/FleetManagerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fleetmanagerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagerSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
